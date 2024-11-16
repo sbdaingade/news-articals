26521fc2771587e3d88ae5c809485794df6c0fb1
sachindaingade <sbdaingade@gmail.com>
2024-11-16 21:18:41 +0530

    details added

--- NewsArticals/NewsArticals/Constants/Constant.swift
+++ NewsArticals/NewsArticals/Constants/Constant.swift
@@ -21,3 +21,25 @@ struct AppConstant {
     
     
 }
+/*
+ 
+ Coding Assignment: News Reader App
+ This assignment evaluates your skills in building a well-architected and modern iOS application. It
+ focuses on utilizing recent trends in iOS development.
+ Application Description:
+ Develop a simple news reader application that fetches news articles from a public API (e.g., News API)
+ and displays them in a user-friendly interface. Users should be able to:
+  Browse a list of news articles with titles and summaries.
+  Read the full content of an article by tapping on it.
+  Bookmarking articles for later reading.
+  Filtering news by category.
+ Technical Requirements:
+  Use latest &amp; best practices in iOS App development.
+ Additional Notes:
+  Feel free to choose any public news API to fetch articles.
+  You can provide a README file with additional information about your implementation choices
+ and testing strategy.
+ This assignment aims to assess your ability to leverage the latest trends in iOS development while
+ building a well-architected and maintainable application. Good Luck
+ 
+ */
