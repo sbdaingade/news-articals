38421078f1c37b85f4a6a5bfd89a84e3e7ced6a1
sachindaingade <sbdaingade@gmail.com>
2024-12-05 00:10:14 +0530

    new custom changes added

---
+++ NewsArticals/NewsArticals/CustomView/CustomTabSelectionView.swift
@@ -0,0 +1,106 @@
+//
+//  CustomTabSelectionView.swift
+//  NewsArticals
+//
+//  Created by Sachin Daingade on 04/10/24.
+//
+
+import SwiftUI
+
+enum SDButtonState {
+    case normal
+    case selected
+    
+    func getButtonStateColor() -> Color {
+        switch self {
+        case .normal:
+            return Color(.orange)
+        case .selected:
+            return Color(.green)
+        }
+    }
+    
+    func getButtonTappedColor() -> Color {
+        switch self {
+        case .normal:
+            return Color(.orange)
+        case .selected:
+            return Color(.green)
+        }
+    }
+}
+
+struct CustomTabButtonView: View {
+    @State private var isPressed: Bool = false
+    let text: String
+    let iconName: String
+    var buttonState: SDButtonState = .normal
+    var action: () -> Void
+    
+    var body: some View {
+        ZStack {
+            Button {
+                
+            } label: {
+                SDTabbarButtonImage(systemImageName: iconName, state: buttonState, isPressed: true)
+                Text(text)
+                    .foregroundColor(buttonState.getButtonStateColor())
+                
+            } .pressEvents {
+                withAnimation(.easeOut(duration: 0.1)) {
+                    isPressed = true
+                }
+            } onRelease: {
+                withAnimation(.easeOut(duration: 0.1)) {
+                    isPressed = true
+                    action()
+                }
+            }
+        }
+    }
+}
+
+struct SDTabbarButtonImage: View {
+    let systemImageName: String
+    var state: SDButtonState = .normal
+    var isPressed: Bool = false
+    init(systemImageName: String, state:  SDButtonState, isPressed: Bool = false) {
+        self.systemImageName = systemImageName
+        self.state = state
+        self.isPressed = isPressed
+    }
+    var body: some View {
+        Image(systemName:systemImageName)
+            .resizable()
+            .renderingMode(.template)
+            .foregroundColor(isPressed ? state.getButtonTappedColor() : state.getButtonTappedColor())
+            .frame (width: 60,height: 20)
+    }
+}
+
+struct ButtonPress: ViewModifier {
+    var onPress: () -> Void
+    var onRelease: () -> Void
+    func body (content: Content) -> some View {
+        content
+            .simultaneousGesture (
+                DragGesture (minimumDistance: 0)
+                    .onChanged ({_ in
+                        onPress ( )
+                    })
+                    .onEnded ( { _ in onRelease ( )
+                    })
+            )
+    }
+}
+
+extension View {
+    func pressEvents(onPress: @escaping (() -> Void), onRelease: @escaping (() -> Void)) -> some View {
+        modifier (ButtonPress(onPress: {
+            onPress()
+        }, onRelease: {
+            onRelease()
+        }))
+    }
+}
+
--- NewsArticals/NewsArticals/Views/MainView.swift
+++ NewsArticals/NewsArticals/Views/MainView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 import CoreData
 
+
+
+enum TabViewState {
+    case newArticle
+    case search
+    case bookmark
+    
+    func getTabbarButtonImage(tabIndex: Int) -> String {
+        switch self {
+        case .newArticle:
+            return tabIndex == 0 ? "house" :  "house.fill"
+        case .search:
+            return tabIndex == 1 ? "magnifyingglass" :  "magnifyingglass.circle.fill"
+        case .bookmark:
+            return tabIndex == 2 ? "bookmark" :  "bookmark.fill"
+        }
+    }
+    
+    func getTabTitle() -> String {
+        switch self {
+        case .newArticle:
+            return "New Articles"
+        case .search:
+            return "Search"
+        case .bookmark:
+            return "Bookmarks"
+        }
+    }
+    
+    func getTabIndex() ->Int {
+        switch self {
+        case .newArticle:
+            return 0
+        case .search:
+            return 1
+        case .bookmark:
+            return 2
+        }
+    }
+}
+
+
+
 struct MainView: View {
-    @State var tabSelection: Int = 0
+    @State var tabSelection: Int = 1
+    
     @Environment(\.managedObjectContext) private var viewContext
     init() {
         UITabBar.appearance().backgroundColor = UIColor.init(white: 0.8, alpha: 0.7)
@@ -18,22 +62,30 @@ struct MainView: View {
         TabView(selection: $tabSelection) {
             NewArticleView(tabselection: $tabSelection)
                 .tabItem {
-                    Image(systemName: "book.pages")
-                    Text("New Articles")
+                    tabView(tabType: .newArticle)
                 }.tag(0)
             
             SearchView(tabselection: $tabSelection)
                 .tabItem {
-                    Image(systemName:"magnifyingglass")
-                    Text("Search")
-                }.tag(0)
+                    tabView(tabType: .search)
+                }.tag(1)
             
             BookmarksView(tabselection: $tabSelection)
                 .tabItem {
-                    Image(systemName:"bookmark")
-                    Text("Bookmarks")
-                }.tag(0)
+                    tabView(tabType: .bookmark)
+                }.tag(2)
+            
+                .onChange(of: tabSelection, {
+                    debugPrint("Index selected \(tabSelection)")
+                })
+                
         }
+    }
+    
+    func tabView(tabType: TabViewState) -> some View {
+        return CustomTabButtonView(text: tabType.getTabTitle(), iconName: tabType.getTabbarButtonImage(tabIndex: tabSelection), buttonState: tabSelection == tabType.getTabIndex() ? .normal : .selected, action: {
+            debugPrint("Index selected ere \(tabType.getTabIndex())")
+        })
     }
 }
 
