a00387a2a121bdb924c42a987dcfab52808aff06
sachindaingade <sbdaingade@gmail.com>
2025-02-23 18:03:55 +0530

    search bar added

--- NewsArticals/NewsArticals/Views/SearchView.swift
+++ NewsArticals/NewsArticals/Views/SearchView.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 struct SearchView: View {
     @Binding var tabselection: Int
-
+    @State private var searchText = ""
+    let items = ["Swift", "Kotlin", "Java", "Python", "C++"]
+    
+    var filteredItems: [String] {
+        searchText.isEmpty ? items : items.filter { $0.localizedCaseInsensitiveContains(searchText) }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                TextField("Search news articles", text: $searchText)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                
+                if !searchText.isEmpty {
+                    Button(action: { searchText = "" }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            List(filteredItems, id: \.self) { item in
+                Text(item)
+            }
+        }
+        .navigationTitle("Languages")
     }
 }
 
